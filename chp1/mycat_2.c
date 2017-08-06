81f0c604ca4ef491240b1fda5553db7b70858641
normalAdam <szd1007@gmail.com>
2017-08-06 23:40:08 +0800

    cat

---
+++ chp1/mycat_2.c
@@ -0,0 +1,20 @@
+#include "apue.h"
+
+int main(void)
+{
+    int     c;
+    while ((c = getc(stdin)) !=EOF)
+        if(putc(c, stdout) ==EOF)
+        {
+            printf("output error");
+            exit(-1);
+        }
+    if(ferror(stdin))
+    {
+        printf("input error");
+        exit(-1);
+    }
+
+    exit (0);
+}
+
