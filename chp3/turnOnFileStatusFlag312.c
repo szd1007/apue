9ff7b7d0a5fe5cfba3847ecb3ef600457db76579
normalAdam <szd1007@gmail.com>
2017-10-28 09:36:03 +0800

    turn on file status

---
+++ chp3/turnOnFileStatusFlag312.c
@@ -0,0 +1,24 @@
+#include "apue.h"
+#include <fcntl.h>
+
+/* flags are file status flags to turn on */
+void set_fl(int fd, int flags) 
+{
+    int val;
+
+    if ((val = fcntl(fd, F_GETFL, 0)) < 0)
+        err_sys("fcntl F_GETFL error");
+
+    val |= flags;   /* turn on flags */
+    // val &= ~flags; /* turn off flags */
+
+    if(fcntl(fd, F_SETFL, val) < 0)
+        err_sys("fcntl F_SETFL error");
+}
+int main()
+{
+    // usage ./a.out 666<>test.file
+    set_fl(666, O_SYNC);
+}
+    
+
