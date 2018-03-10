4a22012b8d2f1e9e80f65b456fe7dbb27895422e
normalAdam <szd1007@gmail.com>
2018-03-10 10:12:12 +0800

    end chp7

---
+++ chp7/ex710.c
@@ -0,0 +1,22 @@
+
+int f1(int val)
+{
+    int num = 0;
+    int *ptr = &num;
+
+    if (val == 0) {
+        int val;
+
+        val = 5;
+        ptr = &val;
+    }
+    return(*ptr + 1);
+}
+
+
+
+int main()
+{
+    f1(0);
+}
+
