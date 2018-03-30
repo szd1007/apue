cac0f2ed266813e6ee7f2c4a6324c475465d2d28
normalAdam <szd1007@gmail.com>
2018-03-30 23:04:56 +0800

    echoAllArgs817

--- chp8/exampleOfExec816.c
+++ chp8/exampleOfExec816.c
@@ -7,10 +7,10 @@ int main(void)
 {
     pid_t   pid;
 
-    if ((pid = fork) < 0) {
+    if ((pid = fork()) < 0) {
         err_sys("fork error");
     } else if (pid == 0) {  /* specify pathname, specify environment */
-        if (execle("./echoall", "echoall", "myarg1", "MY_ARG2", (char *)0,
+        if (execle("./echo.out", "echo.out", "myarg1", "MY_ARG2", (char *)0,
                     env_init) < 0)
             err_sys("execle error");
     }
@@ -21,9 +21,11 @@ int main(void)
     if ((pid = fork()) < 0) {
         err_sys("fork_error");
     } else if (pid == 0) { /* specify filename, inherit environment */
-        if (execlp("echoall", "echoall", "only 1 arg", (char *)0) < 0)
+        if (execlp("./echo.out", "echo.out", "only 1 arg", (char *)0) < 0)
             err_sys("execlp error");
     }
+//    if (waitpid(pid, NULL, 0) < 0)
+//        err_sys("wait error");
 
     exit(0);
 }
